b0da08b6cb376ce1da511b3b2939e096e2abfcfa
Chiranjeevi SD <jeevi146@gmail.com>
2018-02-16 15:47:08 -0500

    added mock api and author page

---
+++ src/api/authorData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    authors: [
+        {
+            id: 'akash',
+            firstName: 'Akash',
+            lastName: 'A'
+        },
+        {
+            id: 'rakshit',
+            firstName: 'Rakshith',
+            lastName: 'N'
+        },
+        {
+            id: 'sujay',
+            firstName: 'Sujay',
+            lastName: 'M'
+        }
+    ]
+};
